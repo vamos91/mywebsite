98586bf0021205f0cc60551e73b82deb44b40d48
simplon-marseille <acardnicolas@hotmail.com>
2022-01-12 16:23:22 +0100

    app update

--- src/containers/blog/import.js
+++ src/containers/blog/import.js
@@ -1,6 +1,6 @@
 import blog01 from '../../assets/ouimakeweb.png'
 import blog02 from '../../assets/sca.png'
-import blog03 from '../../assets/gsb.png'
+import blog03 from '../../assets/profile prat.png'
 import blog04 from '../../assets/makemefly.png'
 import blog05 from '../../assets/deezer.png'
 
