eb7248e82a67b61355903ad72fd860d5f947fc52
simplon-marseille <acardnicolas@hotmail.com>
2022-01-21 16:02:27 +0100

    fix fautes

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -22,9 +22,9 @@ const Blog = () => {
                 </div>
                 <div className='gpt3__blog-container-groupB'>
                     <Article imgUrl={blog02} date="12 dec 2018" techno={[< FaVuejs color="#3FB17F" />]} title="Site vitrine permettant la reservation de croisières dans le monde entier. Les skippers peuvent d'ajouter, voir, modifier et supprimer des croisières" />
-                    <Article imgUrl={blog03} date="12 dec 2020" techno={[< FaVuejs color="#3FB17F" />, < SiMysql color="#DD8B00" />]} title="Système de gestion client (CRM) permettant à des commerciaux de gérer leur portfeuille de praticien. Projet pédagogique." />
-                    <Article imgUrl={blog04} date="4 avril dec 2020" techno={[< SiRubyonrails color="#C60700" />, < SiMysql color="#DD8B00" />]} title="Application de mise ne relation entre des pilotes et des passagers. Il s'agit d'un projet de fin d'étude au Wagon." />
-                    <Article imgUrl={blog05} date="20 juillet 2021" techno={[< FaVuejs color="#3FB17F" />, < SiMysql color="#DD8B00" />, < FaPhp color="#5D7DB0" />]} title="Application de musique basée sur l'API de Deezers. Projet pédagogique à destination d'élèves de Bachelor." />
+                    <Article imgUrl={blog03} date="11 oct 2020" techno={[< FaVuejs color="#3FB17F" />, < SiMysql color="#DD8B00" />]} title="Système de gestion client (CRM) permettant à des commerciaux de gérer leur portfeuille de praticien. Projet pédagogique." />
+                    <Article imgUrl={blog04} date="4 avr 2020" techno={[< SiRubyonrails color="#C60700" />, < SiMysql color="#DD8B00" />]} title="Application de mise ne relation entre des pilotes et des passagers. Il s'agit d'un projet de fin d'étude au Wagon." />
+                    <Article imgUrl={blog05} date="20 juil 2021" techno={[< FaVuejs color="#3FB17F" />, < SiMysql color="#DD8B00" />, < FaPhp color="#5D7DB0" />]} title="Application de musique basée sur l'API de Deezers. Projet pédagogique à destination d'élèves de Bachelor." />
                 </div>
             </div>
         </div>
